7c11ea9213d8a9cbb40efd2ec767a799313932e6
jtmaynes01 <may20012@byui.edu>
2024-01-23 06:19:18 -0700

    css almost done

---
+++ scripts/getDate.js
@@ -0,0 +1,8 @@
+// get current year
+const currentYearElement = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+currentYearElement.innerHTML = `&copy ${currentYear}`;
+// get last modified
+const lastModifiedElement = document.querySelector('#lastModified');
+const lastModified = document.lastModified;
+lastModifiedElement.innerHTML = `Last Modified: <em>${lastModified}<em>`;
