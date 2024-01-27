e63a6ca39e6f61999d1cb830ef90466862a5d4fa
jtmaynes01 <may20012@byui.edu>
2024-01-27 16:31:52 -0700

    bom activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,19 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', () => {
-    if (input.value) {
+button.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  if (input.value) {
       const li = document.createElement('li'); 
       const btnDelete = document.createElement('button');
       
       li.textContent = input.value;
         btnDelete .textContent = '❌';
 
+        btnDelete.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        })
         li.appendChild(btnDelete);
 
         list.appendChild(li);
