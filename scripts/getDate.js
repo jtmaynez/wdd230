db35aa6bc803c1ad0e1ae47db1d84e350034e342
jtmaynes01 <may20012@byui.edu>
2024-02-17 15:32:54 -0700

    dark mode applied

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -1,20 +1,29 @@
+/** @format */
+
 // get current year
-const currentYearElement = document.querySelector('#year');
+const currentYearElement = document.querySelector("#year");
 const currentYear = new Date().getFullYear();
 currentYearElement.innerHTML = `&copy ${currentYear}`;
 // get last modified
-const lastModifiedElement = document.querySelector('#lastModified');
+const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = document.lastModified;
 lastModifiedElement.innerHTML = `Last Modified: <em>${lastModified}<em>`;
 
 // Hamburger menu
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
 
 // .dark in css and toggle.
+const modeButton = document.querySelector("#displayBtn");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  main.classList.toggle("dark-mode");
+});
